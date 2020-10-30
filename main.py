c113d1dff505042b7447b0dd6001ecbbf461ca43
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-30 17:56:57 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def exponential(lam):
+  # Your code to generate an exponential random variable goes here
+  
+  
+# Here is some code that just tests your function is working correctly
+print( exponential(2), exponential(2), exponential(2), exponential(2) )
