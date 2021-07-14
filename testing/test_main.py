c17e0225e12b78cd10083259a6fe650840bdb053
Gareth Tribello <gareth.tribello@gmail.com>
2021-07-14 14:50:01 +0100

    Added new version of tests using AssCheck

---
+++ testing/test_main.py
@@ -0,0 +1,22 @@
+try:
+    from AutoFeedback.funcchecks import check_func 
+except:
+    import subprocess
+    import sys
+
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "AutoFeedback"])
+    from AutoFeedback.funcchecks import check_func
+
+from AutoFeedback.randomclass import randomvar
+import unittest
+from main import *
+
+class UnitTests(unittest.TestCase) :
+    def test_lam(self) :
+        inputs, var = [], []
+        for j in range(1,20) : 
+           lam=j*20
+           inputs.append((lam,))
+           myvar1 = randomvar( 1/lam, variance=1/(lam*lam), vmin=0, isinteger=False )
+           var.append(myvar1)
+        assert( check_func("exponential", inputs, var) )
