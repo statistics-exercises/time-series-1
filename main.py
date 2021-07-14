36f09efbd4d7bfc1c31e487a5da1888c0603739f
Gareth Tribello <gareth.tribello@gmail.com>
2021-07-14 15:48:36 +0100

    Added answers

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def exponential(lam):
   # Your code to generate an exponential random variable goes here
-  
+  return -np.log( np.random.uniform(0,1) ) / lam  
   
 # Here is some code that just tests your function is working correctly
 print( exponential(2), exponential(2), exponential(2), exponential(2) )
